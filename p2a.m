a530d737a31ae8b35c7c844c1ad5f4d3a695e9b6
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2024-03-11 13:34:58 +0000

    Alínea a - problema 2 finalizada

---
+++ p2a.m
@@ -0,0 +1,25 @@
+% TRABALHO 1 - Sinais e Sistemas
+clear; close all; clc;
+
+%%Criar pontos
+tn = linspace(-0.004,0.004,1000);
+
+%%x0t
+x0t = 1000*(heaviside(tn + 0.002) - heaviside(tn - 0.002));
+subplot(1,4,1),plot(tn,x0t), grid on, xlabel('t(s)'), title('x0t');
+
+%%x1t
+x1t = (heaviside(tn + 0.001) - heaviside(tn - 0.001)) .* (5.0e5*tn + 500) +...
+    (heaviside(tn - 0.001) - heaviside(tn - 0.003)) .* (-5.0e5*tn + 1500);
+subplot(1,4,2),plot(tn,x1t), grid on, xlabel('t(s)'), title('x1t');
+
+%%x2t
+x2t = x0t .* sign(tn+0.001); 
+subplot(1,4,3), plot(tn,x2t), grid on, xlabel('t(s)'), title('x2t');
+
+%%x3t
+x3t = (heaviside(tn+0.003) - heaviside(tn + 0.001)) .* (-5.0e5*tn - 1500) +...
+    (-500 *(heaviside(tn + 0.001) - heaviside(tn - 0.001))) .* sign(tn) +...
+    (heaviside(tn - 0.001) - heaviside(tn - 0.003)) .* (-5.0e5*tn + 1500);
+subplot(1,4,4), plot(tn, x3t), grid on, xlabel('t(s)'), title('x3t');
+
